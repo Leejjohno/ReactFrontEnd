5940fbbc7867007019ef80c04bdba9aa4db18ff5
Leejjohno <leej67885@gmail.com>
2022-07-25 15:26:46 +0100

    removed username from update email as it's not needed

--- src/components/updateEmail/index.js
+++ src/components/updateEmail/index.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { updateEmail } from "../../utils";
 
 export const UpdateEmail = ( { setUpdateEmail, isLoggedIn } ) => {
-    const [username, setUsername] = useState();
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
 
@@ -14,7 +13,6 @@ export const UpdateEmail = ( { setUpdateEmail, isLoggedIn } ) => {
     return(
         <form onSubmit={submitHandler}>
             <h2>Update your email address here</h2>
-            <input defaultValue={"Username"} onChange={(e) => setUsername(e.target.value)} />
             <input defaultValue={"New Email"} onChange={(e) => setEmail(e.target.value)} />
             <input defaultValue={"Password"} onChange={(e) => setPassword(e.target.value)} />
             <button type="submit">Update</button>
